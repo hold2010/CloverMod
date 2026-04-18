9026fb50c1e641bc4424886a1f981990fbbfdd45
沧海飞尘 <1170010871@qq.com>
2026-04-18 10:57:58 +0800

    update

--- Scripts/CloverRelic.cs
+++ Scripts/CloverRelic.cs
@@ -1,37 +1,142 @@
 using HarmonyLib;
-using MegaCrit.Sts2.Core.CardSelection;
-using MegaCrit.Sts2.Core.Combat;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Creatures;
-using MegaCrit.Sts2.Core.Entities.Merchant;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.Factories;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.Rooms;
+using MegaCrit.Sts2.Core.Runs;
+using MegaCrit.Sts2.Core.Saves.Runs;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Merchant;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Enchantments;
 using MegaCrit.Sts2.Core.Rewards;
-using MegaCrit.Sts2.Core.Rooms;
-using MegaCrit.Sts2.Core.Runs;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace CloverMod;
 
-public class CloverRelic : RelicModel, IPoolModel
+public class CloverRelic : RelicModel
 {
-    public override RelicRarity Rarity { get; } = RelicRarity.Starter;
-    public string EnergyColorName { get; } = "colorless";
-    public decimal DamageReceivedThisTurn { get; set; }
-    public decimal MaxDamageReceivedEveryTurn { get; set; } = 50m;
+    private bool _isActivating;
 
-    //获得遗物之后
-    public override async Task AfterObtained()
-    {
-        
-        Flash(); // 触发遗物图标闪烁
-        await PlayerCmd.GainGold(999, base.Owner, false);
+	private int _combatsSeen;
 
-    }
+	public override RelicRarity Rarity => RelicRarity.Starter;
 
+
+	public override bool ShowCounter => true;
+
+	public override int DisplayAmount
+	{
+		get
+		{
+			if (!IsActivating)
+			{
+				return CombatsSeen % 2;
+			}
+			return 2;
+		}
+	}
+
+	private bool IsActivating
+	{
+		get
+		{
+			return _isActivating;
+		}
+		set
+		{
+			AssertMutable();
+			_isActivating = value;
+			InvokeDisplayAmountChanged();
+		}
+	}
+
+	[SavedProperty]
+	public int CombatsSeen
+	{
+		get
+		{
+			return _combatsSeen;
+		}
+		set
+		{
+			AssertMutable();
+			_combatsSeen = value;
+		}
+	}
+
+	private bool IsInTriggeringCombat
+	{
+		get
+		{
+			if (CombatsSeen > 0)
+			{
+				return CombatsSeen % 2 == 0;
+			}
+			return false;
+		}
+	}
+
+	public override bool IsAllowed(IRunState runState)
+	{
+		return RelicModel.IsBeforeAct3TreasureChest(runState);
+	}
+
+	public override bool TryModifyCardRewardOptions(Player player, List<CardCreationResult> options, CardCreationOptions creationOptions)
+	{
+		if (base.Owner != player)
+		{
+			return false;
+		}
+		if (creationOptions.Source != CardCreationSource.Encounter)
+		{
+			return false;
+		}
+		if (!IsInTriggeringCombat)
+		{
+			return false;
+		}
+		IEnumerable<CardModel> customCardPool = from c in creationOptions.GetPossibleCards(player)
+			where c.Rarity == CardRarity.Rare && options.TrueForAll((CardCreationResult o) => o.originalCard.Id != c.Id)
+			select c;
+        // 单稀有度卡池 → 强制用 CardRarityOddsType.Uniform
+		CardCreationOptions options2 = new CardCreationOptions(customCardPool, CardCreationSource.Other, CardRarityOddsType.Uniform).WithFlags(CardCreationFlags.NoModifyHooks | CardCreationFlags.NoCardPoolModifications);
+		CardModel cardModel = CardFactory.CreateForReward(base.Owner, 1, options2).FirstOrDefault()?.Card;
+		if (cardModel != null)
+		{
+			CardCreationResult cardCreationResult = new CardCreationResult(cardModel);
+			cardCreationResult.ModifyCard(cardModel, this);
+			options.Add(cardCreationResult);
+		}
+		return cardModel != null;
+	}
+
+	public override Task AfterCombatEnd(CombatRoom room)
+	{
+		CombatsSeen++;
+		if (IsInTriggeringCombat)
+		{
+			TaskHelper.RunSafely(DoActivateVisuals());
+		}
+		InvokeDisplayAmountChanged();
+		return Task.CompletedTask;
+	}
+
+	private async Task DoActivateVisuals()
+	{
+		IsActivating = true;
+		Flash();
+		await Cmd.Wait(1f);
+		IsActivating = false;
+	}
   
 }
